b181033f6701f5ad544b4ca4cc58aa46ba5753f0
Jakub Zehner <kubusja.pl@gmail.com>
2023-01-16 18:55:54 +0100

    Add rfid read

--- client.py
+++ client.py
@@ -22,8 +22,7 @@ def main():
     broker_address = sys.argv[1] if len(sys.argv) > 1 else LOCALHOST
     rfid_reader = RfidReader()
     # TODO: print message that we are waiting for rfid
-    rfid_reader.wait_for_read()
-    client_id = rfid_reader.get_uid()
+    client_id = rfid_reader.read_uid()
 
     with ClientConnection(broker_address, client_id) as connection:
         game_loop(connection, display)
