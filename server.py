e581972be42a4fdb11613c3efe03f3830dd9c47e
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-01-16 18:59:41 +0100

    Select player colors

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from connection import ServerConnection
 from board import Board, Tile
 
 
-def wait_for_players(connection: ServerConnection) -> None:
+def wait_for_players(connection: ServerConnection) -> dict[Tile, str]:
     print("[INFO] Waiting for players...")
-    clients = set()
+    clients: set[str] = set()
 
     while len(clients) < 2:
         message = connection.receive_message()
@@ -14,21 +14,33 @@ def wait_for_players(connection: ServerConnection) -> None:
             case "disconnected":
                 clients.discard(message.sender)
 
+    white_uid = clients.pop()
+    black_uid = clients.pop()
+    return {Tile.WHITE: white_uid, Tile.BLACK: black_uid}
 
-def game_loop(connection: ServerConnection):
+
+def game_loop(connection: ServerConnection, players: dict[Tile, str]):
     print("[INFO] Starting the game!")
     board = Board()
 
     while board.winner() == Tile.EMPTY:
         connection.broadcast("board", board.serialize())
-        # TODO: wait for player input
-        connection.receive_message()
+
+        message = connection.receive_message()
+        match message.tag:
+            case "disconnected":
+                if message.sender in players.values:
+                    pass
+                    # TODO: game over, second player wins
+            case "place":
+                pass
+                # TODO: place tile on board
 
 
 def main():
     with ServerConnection() as connection:
-        wait_for_players(connection)
-        game_loop(connection)
+        players = wait_for_players(connection)
+        game_loop(connection, players)
 
 
 if __name__ == "__main__":
